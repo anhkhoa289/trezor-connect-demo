c6c683091c702f00aa23dfda43763f3d6db2ed17
anh khoa <khoa@example.com>
2018-10-19 10:45:29 +0700

    test

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express')
+const app = express()
 const TrezorConnect = require('trezor-connect').default
 
+const port = 3000
 
-TrezorConnect.getAddress('Account #1').then(console.log)
+app.get('/', function (req, res) {
+    TrezorConnect.getPublicKey({
+        path: "m/49'/0'/0'",
+        coin: "btc"
+    }).then(res.send)
+})
+
+app.listen(port, () => console.log(`Khoa app listening on port ${port}!`))
+
